a24d30baeeb13239baa173eadfddaea0986792ca
Pixenite <90865040+PaulMares@users.noreply.github.com>
2024-05-12 13:10:34 -0700

    Not working, test mask with camshift

--- python/opencvTest.py
+++ python/opencvTest.py
@@ -5,6 +5,8 @@ img = cv.imread("C:\\Users\\FabienMaresquier\\Pictures\\img003.jpg")
 
 cap = cv.VideoCapture(0)
 
+backSub = cv.createBackgroundSubtractorMOG2()
+
 # take first frame of the video
 ret,frame = cap.read()
 # setup initial location of window
@@ -12,14 +14,12 @@ x, y, w, h = 300, 230, 100, 100 # simply hardcoded the values
 track_window = (x, y, w, h)
 # set up the ROI for tracking
 roi = frame[y:y+h, x:x+w]
-hsv_roi = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
-mask = cv.inRange(hsv_roi, np.array((220., 220.,220.)), np.array((255.,255.,255.)))
+hsv_roi = backSub.apply(roi)
+mask = cv.inRange(hsv_roi, np.array(200.), np.array(255.))
 roi_hist = cv.calcHist([hsv_roi],[0],mask,[180],[0,180])
 cv.normalize(roi_hist,roi_hist,0,255,cv.NORM_MINMAX)
 # Setup the termination criteria, either 10 iteration or move by at least 1 pt
 term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1 )
-
-backSub = cv.createBackgroundSubtractorMOG2()
 
 while True:
     retval, frame = cap.read()
@@ -29,8 +29,8 @@ while True:
     cv.imshow("Live", frame)
     frame = backSub.apply(frame)
 
-    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    dst = cv.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+    hsv = frame
+    dst = cv.calcBackProject([hsv], [0], roi_hist, [100, 255], 1)
     # apply camshift to get the new location
     ret, track_window = cv.CamShift(dst, track_window, term_crit)
     # Draw it on image
