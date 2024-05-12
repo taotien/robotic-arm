ae700e251e7745e84fd462afbd9cb79a46dc2f32
Pixenite <90865040+PaulMares@users.noreply.github.com>
2024-05-12 13:01:26 -0700

    Testing mask

--- python/opencvTest.py
+++ python/opencvTest.py
@@ -13,11 +13,13 @@ track_window = (x, y, w, h)
 # set up the ROI for tracking
 roi = frame[y:y+h, x:x+w]
 hsv_roi = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
-mask = cv.inRange(hsv_roi, np.array((10., 60.,32.)), np.array((140.,140.,140.)))
+mask = cv.inRange(hsv_roi, np.array((220., 220.,220.)), np.array((255.,255.,255.)))
 roi_hist = cv.calcHist([hsv_roi],[0],mask,[180],[0,180])
 cv.normalize(roi_hist,roi_hist,0,255,cv.NORM_MINMAX)
 # Setup the termination criteria, either 10 iteration or move by at least 1 pt
 term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1 )
+
+backSub = cv.createBackgroundSubtractorMOG2()
 
 while True:
     retval, frame = cap.read()
@@ -25,6 +27,7 @@ while True:
         continue
 
     cv.imshow("Live", frame)
+    frame = backSub.apply(frame)
 
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     dst = cv.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
