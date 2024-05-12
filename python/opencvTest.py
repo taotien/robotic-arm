0f7b4aa5fed6dedc2e39ce9c7a0accb50ec4024a
Pixenite <90865040+PaulMares@users.noreply.github.com>
2024-05-12 12:32:55 -0700

    Testing meanshift

--- python/opencvTest.py
+++ python/opencvTest.py
@@ -5,20 +5,37 @@ img = cv.imread("C:\\Users\\FabienMaresquier\\Pictures\\img003.jpg")
 
 cap = cv.VideoCapture(0)
 
-frame = np.array([], dtype=int)
-
-backSub = cv.createBackgroundSubtractorMOG2()
+# take first frame of the video
+ret,frame = cap.read()
+# setup initial location of window
+x, y, w, h = 300, 200, 100, 50 # simply hardcoded the values
+track_window = (x, y, w, h)
+# set up the ROI for tracking
+roi = frame[y:y+h, x:x+w]
+hsv_roi = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
+mask = cv.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+roi_hist = cv.calcHist([hsv_roi],[0],mask,[180],[0,180])
+cv.normalize(roi_hist,roi_hist,0,255,cv.NORM_MINMAX)
+# Setup the termination criteria, either 10 iteration or move by at least 1 pt
+term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1 )
 
 while True:
     retval, frame = cap.read()
-    if frame is None:
+    if retval is False:
         continue
 
-    fgMask = backSub.apply(frame)
-
     cv.imshow("Live", frame)
-    cv.imshow("Mask", fgMask)
+
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    dst = cv.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+    # apply meanshift to get the new location
+    ret, track_window = cv.meanShift(dst, track_window, term_crit)
+    # Draw it on image
+    x, y, w, h = track_window
+    img2 = cv.rectangle(frame, (x, y), (x + w, y + h), 255, 2)
+    cv.imshow('img2', img2)
 
     keyboard = cv.waitKey(1)
     if keyboard == 'q' or keyboard == 27:
+
         break
